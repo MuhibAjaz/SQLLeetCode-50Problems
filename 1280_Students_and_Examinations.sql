020af282c6d51fdd58ad5bb9c48bccc9d6edc146
MuhibAjaz <136195675+MuhibAjaz@users.noreply.github.com>
2024-10-15 15:52:04 +0500

    1280_Students_and_Examinations.sql
    The result table should contain all students and all subjects.
    Alice attended the Math exam 3 times, the Physics exam 2 times, and the Programming exam 1 time.
    
    Bob attended the Math exam 1 time, the Programming exam 1 time, and did not attend the Physics exam.
    
    Alex did not attend any exams.
    
    John attended the Math exam 1 time, the Physics exam 1 time, and the Programming exam 1 time.

---
+++ 1280_Students_and_Examinations.sql
@@ -0,0 +1,13 @@
+select s.student_id,s.student_name,sub.subject_name,count(e.subject_name) as attended_exams 
+from 
+Students s 
+cross join 
+Subjects as sub
+left join 
+Examinations e
+on 
+s.student_id=e.student_id
+and 
+sub.subject_name=e.subject_name
+group by s.student_id,s.student_name,sub.subject_name
+order by s.student_id ,s.student_id 
